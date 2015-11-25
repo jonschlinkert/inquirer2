9706ffe3ffb7093b99f441ee2caad56608dc1627
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-25 06:53:26 -0500

    start fixing inconsistencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,26 @@
 /*jshint strict:false */
-module.exports = function( grunt ) {
 
+module.exports = function (grunt) {
   grunt.initConfig({
-
     jshint: {
       options: {
-        jshintrc : ".jshintrc"
+        jshintrc: '.jshintrc'
       },
-      files: [ "lib/**/*.js", "test/**/*.js" ]
+      files: ['lib/**/*.js', 'test/**/*.js']
     },
 
     mochaTest: {
       options: {
-        ui: "bdd",
-        reporter: "spec",
-        require: "test/before.js"
+        ui: 'bdd',
+        reporter: 'spec',
+        require: 'test/before.js'
       },
-      all: "test/specs/**/*.js"
+      all: 'test/specs/**/*.js'
     }
 
   });
 
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-mocha-test");
-
-  grunt.registerTask("default", [ "jshint", "mochaTest" ]);
-
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('default', ['mochaTest']);
 };
